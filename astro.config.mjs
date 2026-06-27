b389950cf8677ee5f76aaa66f446e581b03dd7ab
Ramón <rpgseo7@gmail.com>
2026-06-27 13:42:13 +0200

    chore: upgrade Astro and dependencies, and configure Cloudflare adapter for Node environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'static',
   site: 'https://frenzycars.com',
-  adapter: cloudflare(),
+  adapter: cloudflare({ prerenderEnvironment: 'node' }),
   trailingSlash: 'always',
   build: { format: 'directory' },
   integrations: [sitemap(), react()],
